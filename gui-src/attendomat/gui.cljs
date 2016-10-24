2decfbdaa1d2344de0d48f3c96d567ede9b94c5f
Arne Brasseur <arne@arnebrasseur.net>
2016-10-24 17:02:07 +0200

    First steps for the GUI

--- gui-src/attendomat/gui.cljs
+++ gui-src/attendomat/gui.cljs
@@ -1,3 +1,23 @@
-(ns attendomat.gui)
+(ns attendomat.gui
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
 
-(js/alert "gui!")
+(defn checkbox [{:keys [caption]}]
+  (let [id (gensym "checkbox_")]
+    [:label {:for id}
+     [:input {:type "checkbox" :name id :id id}]
+     caption]))
+
+(defn filters []
+  [:div
+   [checkbox {:caption "show all"}]
+   [checkbox {:caption "invited"}]
+   [checkbox {:caption "accepted"}]
+   [checkbox {:caption "cancelled"}]
+   [checkbox {:caption "timeout"}]
+   [checkbox {:caption "not invited"}]])
+
+(defn gui []
+  [filters])
+
+(r/render [gui] (js/document.getElementById "attendomat"))
